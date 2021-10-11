576968e15d2adff41283a689606ff177d56445c7
François <francois.lannoy@kelindi.com>
2021-10-11 16:37:53 +0200

    :white_check_mark: made test pass

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,20 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
+using Fibonacci;
 
-class Program
-{
-    
-    public static async Task Main(string[] args)
-    {
-        Stopwatch stopwatch = new();
-        stopwatch.Start();
-        var tasks = await Fibonacci.Compute.ExecuteAsync(args);
+Stopwatch stopwatch = new();
+stopwatch.Start();
 
-        foreach (var task in tasks) Console.WriteLine($"Fibo result: {task}");
-        stopwatch.Stop();
-        Console.WriteLine($"{stopwatch.Elapsed.Seconds} s");
-    }
-}
+using var fibonacciDataContext = new FibonacciDataContext();
+
+var tasks = await new Fibonacci.Compute(fibonacciDataContext).ExecuteAsync(args);
+
+foreach (var task in tasks) Console.WriteLine($"Fibo result: {task}");
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed.Seconds} s");
