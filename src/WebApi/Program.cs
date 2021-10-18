d9d1a9141e297f2d5b5525f7d1f90b61ee91ff75
Un-dev <francois.lannoy@kelindi.com>
2021-10-18 17:00:53 +0200

    implemented webapi

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,10 @@
+using Fibonacci;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FibonacciDataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<Compute>();
 
 var app = builder.Build();
 
